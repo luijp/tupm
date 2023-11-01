4af69abfb7b89dec3a99b0a62f70f04b2aa040fe
luijp <luijunpeng@outlook.com>
2023-11-02 01:37:22 +0800

    edit: Error code more details

--- src/main/java/cn/luijp/tupm/Enum/ReposCodeEnum.java
+++ src/main/java/cn/luijp/tupm/Enum/ReposCodeEnum.java
@@ -8,12 +8,19 @@ public enum ReposCodeEnum {
     PAGE_NOT_FOUND(404),
     //上传文件为空
     FILE_IS_EMPTY(-10),
+    //指定ID配置文件未找到
     SIT_CONFIG_NOT_FOUND(-11),
+    //删除配置文件失败
     SITE_CONFIG_DELETE_FAILED(-12),
+    //创建配置文件失败
     SITE_CONFIG_CREATE_FAILED(-13),
+    //文件上传失败
     FILE_UPLOAD_FAILED(-14),
+    //指定储存策略未找到
     STORAGE_CONFIG_GET_BY_ID_FAILED(-15),
+    //删除储存策略失败
     STORAGE_CONFIG_DELETE_FAILED(-16),
+    //插入储存策略失败
     STORAGE_CONFIG_INSERT_FAILED(-17)
     ;
 
