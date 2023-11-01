2f4b6d16042bc820bed197753fd46c6035623254
luijp <luijunpeng@outlook.com>
2023-11-01 16:56:20 +0800

    init: init project

---
+++ src/main/java/cn/luijp/tupm/TupmApplication.java
@@ -0,0 +1,13 @@
+package cn.luijp.tupm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TupmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TupmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/luijp/tupm/TupmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.luijp.tupm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TupmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
