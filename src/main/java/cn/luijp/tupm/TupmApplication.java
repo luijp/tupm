86e41e5e5775af7d312e7e1f524994db1de2fd51
luijp <luijunpeng@outlook.com>
2023-11-01 22:53:33 +0800

    add: mybatis-plus support

--- src/main/java/cn/luijp/tupm/TupmApplication.java
+++ src/main/java/cn/luijp/tupm/TupmApplication.java
@@ -1,9 +1,11 @@
 package cn.luijp.tupm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class TupmApplication {
 
     public static void main(String[] args) {
