e075fdeb1da0f9b4a8f95b4e23617ddcad922d53
luijp <luijunpeng@outlook.com>
2023-11-02 18:00:06 +0800

    add: thymeleaf support

---
+++ src/main/java/cn/luijp/tupm/Controller/Admin/Index.java
@@ -0,0 +1,18 @@
+package cn.luijp.tupm.Controller.Admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+
+    @GetMapping("/admin")
+    public String AdminIndex(Model model){
+        return "admin/index";
+
+    }
+
+
+}
